c74cb224d3236208966970f0e787e09581951a28
Marcelo Camillo <marcelocamillo18@gmail.com>
2026-04-10 14:05:58 -0300

    Organizando os componentes em pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-        maiores ea porro dignissimos? Voluptate quibusdam quasi ad at adipisci
-        quia ex eaque sint totam qui! Minus ad dolor voluptas. Saepe.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
