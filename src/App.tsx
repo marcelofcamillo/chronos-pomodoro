2a666978d150e765f2b4885a3b8e70a2ea8d2c39
Marcelo Camillo <marcelocamillo18@gmail.com>
2026-04-10 00:17:59 -0300

    Componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>Olá mundo!</Heading>
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
 
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,9 @@
 import styles from './Heading.module.css';
 
-export function Heading(props) {
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading(props: HeadingProps) {
   return <h1 className={styles.heading}>{props.children}</h1>;
 }
